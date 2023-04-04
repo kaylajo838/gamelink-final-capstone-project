505e593c02f825d38f6380234c5606e2abb7a020
Kayla Imming <kaylaimming@gmail.com>
2023-04-04 14:02:58 -0700

    Add sign in page

---
+++ src/views/SignIn.js
@@ -0,0 +1,159 @@
+import { useState, useEffect } from 'react';
+import "./SignUp.css"
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
+import Link from '@mui/material/Link';
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { auth, db } from '../firebase'
+import { signInWithEmailAndPassword, updateProfile, fetchSignInMethodsForEmail } from 'firebase/auth';
+import { doc, setDoc } from "firebase/firestore";
+import { useNavigate } from 'react-router-dom';
+
+function Copyright(props) {
+  return (
+    <Typography variant="body2" color="text.secondary" align="center" {...props}>
+      {'Copyright © '}
+      <Link color="inherit" href="https://mui.com/">
+        Kayla Imming
+      </Link>{' '}
+      {new Date().getFullYear()}
+    </Typography>
+  );
+}
+
+const theme = createTheme();
+
+export default function SignUp() {
+
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+  const navigate = useNavigate()
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    const data = new FormData(event.currentTarget);
+    console.log({
+      email: data.get("email"),
+      password: data.get("password"),
+    });
+
+    signInWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
+        // Signed in 
+        const user = userCredential.user;
+        // ...
+        navigate('/')
+    })
+    .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+    });
+  };
+
+
+  return (
+    <div className="signup-main-body-container">
+      <div className="signup-container">
+        <div className='signup-form-container'>
+          <ThemeProvider theme={theme}>
+            <Container component="main" maxWidth="xs">
+              <CssBaseline />
+              <Box
+                sx={{
+                  marginTop: 1,
+                  display: "flex",
+                  flexDirection: "column",
+                  alignItems: "center",
+                }}
+              >
+                <Avatar sx={{ m: 1, bgcolor: "#8e3dc6" }}>
+                  <LockOutlinedIcon />
+                </Avatar>
+                <Typography component="h1" variant="h5">
+                  Sign In
+                </Typography>
+                <Box
+                  component="form"
+                  noValidate
+                  onSubmit={handleSubmit}
+                  sx={{ mt: 3 }}
+                >
+                  <Grid container spacing={2}>
+                    <Grid item xs={12}>
+                      <TextField
+                        required
+                        fullWidth
+                        id="email"
+                        label="Email Address"
+                        name="email"
+                        autoComplete="email"
+                        onChange={(event) => {setEmail(event.target.value)}}
+                        sx={{
+                            input: { color: "white" },
+                            fieldset: { borderColor: "#2ca627", borderWidth: "2px" },
+                            "& .MuiOutlinedInput-root": {
+                              "&:hover fieldset": {
+                                borderColor: "#2ca627",
+                              },
+                            },
+                            "& label": {
+                              color: "#2ca627",
+                              fontWeight: 500
+                            },
+                          }}
+                      />
+                    </Grid>
+                    <Grid item xs={12}>
+                      <TextField
+                        required
+                        fullWidth
+                        name="password"
+                        label="Password"
+                        type="password"
+                        id="password"
+                        autoComplete="new-password"
+                        onChange={(event) => {setPassword(event.target.value)}}
+                        sx={{
+                            input: { color: "white" },
+                            fieldset: { borderColor: "#2ca627", borderWidth: "2px" },
+                            "& .MuiOutlinedInput-root": {
+                              "&:hover fieldset": {
+                                borderColor: "#2ca627",
+                              },
+                            },
+                            "& label": {
+                              color: "#2ca627",
+                              fontWeight: 500
+                            },
+                          }}
+                      />
+                    </Grid>
+                  </Grid>
+                  <Button
+                    type="submit"
+                    fullWidth
+                    variant="contained"
+                    sx={{ mt: 3, mb: 2, width: "fit-content", pr: 3, pl: 3 }}
+                  >
+                    Sign Up
+                  </Button>
+                </Box>
+              </Box>
+              <Copyright sx={{ mt: 2, color: "white" }} />
+            </Container>
+          </ThemeProvider>
+        </div>
+      </div>
+    </div>
+  );
+}
