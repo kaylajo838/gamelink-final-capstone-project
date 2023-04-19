cd21ad6ea188aa29f53aae257e21ddeb6d57c5ae
Kayla Imming <kaylaimming@gmail.com>
2023-04-19 11:07:03 -0700

    Incorporated re-authenication with mui Dialog so users can update their email and password with firebase

--- src/views/EditProfile.js
+++ src/views/EditProfile.js
@@ -15,7 +15,7 @@ import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 import { auth, db } from '../firebase'
-import { updatePassword, updateProfile, updateEmail, getAuth } from 'firebase/auth';
+import { updatePassword, updateProfile, updateEmail, getAuth, reauthenticateWithCredential, EmailAuthProvider } from 'firebase/auth';
 import { doc, setDoc } from "firebase/firestore";
 import { useNavigate } from 'react-router-dom';
 import Alert from '@mui/material/Alert';
@@ -24,6 +24,7 @@ import PhotoCamera from '@mui/icons-material/PhotoCamera';
 // import Avatar from '@mui/material/Avatar';
 import Snackbar from '@mui/material/Snackbar';
 import { getStorage, ref, uploadBytesResumable } from 'firebase/storage';
+import { Dialog, DialogTitle, DialogContent, DialogActions} from "@mui/material";
 
 
 export default function EditProfile() {
@@ -36,7 +37,10 @@ export default function EditProfile() {
   const [avatarUrl, setAvatarUrl] = useState('')
   const [alertOpen, setAlertOpen] = useState(false);
   const [alertSeverity, setAlertSeverity] = useState('');
-  const [alertMessage, setAlertMessage] = useState(''); 
+  const [alertMessage, setAlertMessage] = useState('');
+  const [reauthDialogOpen, setReauthDialogOpen] = useState(false); 
+  const [dialogCallback, setDialogCallback] = useState(null);
+
 
   const theme = createTheme();
 
@@ -47,42 +51,76 @@ export default function EditProfile() {
     const user = auth.currentUser;
   
     if (user) {
-      try {
-        const currentDisplayName = user.displayName || "";
-        const currentFirstName = currentDisplayName.split(" ")[0] || "";
-        const currentLastName = currentDisplayName.split(" ")[1] || "";
-
-        const newFirstName = firstName !== "" ? firstName : currentFirstName;
-        const newLastName = lastName !== "" ? lastName : currentLastName;
-
-        await updateProfile(user, {
-        displayName: newFirstName + " " + newLastName,
-        });
-  
-        if (password !== "") {
-          await updatePassword(user, password);
+        try {
+          const currentDisplayName = user.displayName || "";
+          const currentFirstName = currentDisplayName.split(" ")[0] || "";
+          const currentLastName = currentDisplayName.split(" ")[1] || "";
+    
+          const newFirstName = firstName !== "" ? firstName : currentFirstName;
+          const newLastName = lastName !== "" ? lastName : currentLastName;
+    
+          await updateProfile(user, {
+            displayName: newFirstName + " " + newLastName,
+          });
+    
+          if (password !== "") {
+            openReauthDialog(async () => {
+              await updatePassword(user, password);
+            });
+          }
+          
+          if (email !== "") {
+            openReauthDialog(async () => {
+              await updateEmail(user, email);
+            });
+          }
+    
+          console.log("User profile updated successfully");
+          setAlertSeverity("success");
+          setAlertMessage("Successfully Updated Profile!");
+          setAlertOpen(true);
+        } catch (error) {
+          console.error("Error updating user profile:", error.message);
+          setAlertSeverity("error");
+          setAlertMessage("Updating Profile Failed");
+          setAlertOpen(true);
         }
-        if (email !== "") {
-            await updateEmail(user, email)
-        }
-  
-        console.log("User profile updated successfully");
-        setAlertSeverity("success");
-        setAlertMessage("Successfully Updated Profile!");
-        setAlertOpen(true);
-      } catch (error) {
-        console.error("Error updating user profile:", error.message);
-        setAlertSeverity("error");
-        setAlertMessage("Updating Profile Failed");
-        setAlertOpen(true);
+      } else {
+        console.error("User is not authenticated");
       }
-    } else {
-      console.error("User is not authenticated");
-    }
-  };
+    };
+
 
   const handleCloseAlert = () => {
     setAlertOpen(false);
+  };
+
+
+  const handleReauthentication = async (email, password) => {
+    const auth = getAuth();
+    const user = auth.currentUser;
+    const credential = EmailAuthProvider.credential(email, password);
+  
+    try {
+      await reauthenticateWithCredential(user, credential);
+      console.log("User re-authenticated.");
+      setReauthDialogOpen(false);
+  
+      if (dialogCallback) {
+        await dialogCallback();
+        setDialogCallback(null);
+      }
+    } catch (error) {
+      console.error("Error re-authenticating user:", error.message);
+      setAlertSeverity("error");
+      setAlertMessage("Re-authentication Failed");
+      setAlertOpen(true);
+    }
+  };
+
+  const openReauthDialog = (callback) => {
+    setReauthDialogOpen(true);
+    setDialogCallback(() => callback);
   };
 
   const handleImageChange = async (event) => {
@@ -103,7 +141,7 @@ export default function EditProfile() {
       const uploadTask = uploadBytesResumable(storageRef, file);
       uploadTask.on('state_changed',
         (snapshot) => {
-          // You can show upload progress here
+          
         },
         (error) => {
           console.error('Error uploading image:', error.message);
@@ -113,16 +151,60 @@ export default function EditProfile() {
           const downloadURL = await uploadTask.snapshot.ref.getDownloadURL();
           setImageUrl(downloadURL);
   
-          // This is the new line to update the Navbar avatarUrl state
-          // after the image is uploaded.
           setAvatarUrl(downloadURL);
         }
       );
     }
   };
 
+  const reauthDialog = (
+    <Dialog open={reauthDialogOpen} onClose={() => setReauthDialogOpen(false)}>
+      <DialogTitle>Please Enter Your Current Email and Password to Update</DialogTitle>
+      <DialogContent>
+        <TextField
+          autoFocus
+          margin="dense"
+          id="email"
+          label="Email Address"
+          type="email"
+          fullWidth
+          variant="outlined"
+          value={email}
+          onChange={(event) => setEmail(event.target.value)}
+          sx={{
+            '& input:-webkit-autofill, & input:-webkit-autofill:hover, & input:-webkit-autofill:focus, & input:-webkit-autofill:active': {
+              WebkitTextFillColor: 'black',
+              transition: 'background-color 5000s ease-in-out 0s',
+            },
+          }}
+        />
+        <TextField
+          margin="dense"
+          id="password"
+          label="Password"
+          type="password"
+          fullWidth
+          variant="outlined"
+          value={password}
+          onChange={(event) => setPassword(event.target.value)}
+          sx={{
+            '& input:-webkit-autofill, & input:-webkit-autofill:hover, & input:-webkit-autofill:focus, & input:-webkit-autofill:active': {
+              WebkitTextFillColor: 'black',
+              transition: 'background-color 5000s ease-in-out 0s',
+            },
+          }}
+        />
+      </DialogContent>
+      <DialogActions>
+        <Button onClick={() => setReauthDialogOpen(false)}>Cancel</Button>
+        <Button onClick={() => handleReauthentication(email, password)}>Re-authenticate</Button>
+      </DialogActions>
+    </Dialog>
+  );
+
   return (
     <div className="signup-main-body-container">
+        {reauthDialog}
       <div className="signup-container">
         <div className='signup-form-container'>
           <ThemeProvider theme={theme}>
