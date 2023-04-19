7a05e3d41a857bd5f1b36ebe5fc7142d1bcce4a0
Kayla Imming <kaylaimming@gmail.com>
2023-04-19 16:03:30 -0700

    Add layout and logic for profile page, displaying profile image, name, email, number of wishlist items, and platforms all connected to the database

---
+++ src/views/Profile.js
@@ -0,0 +1,148 @@
+import { useState, useEffect } from 'react'
+import "./Profile.css"
+import Avatar from '@mui/material/Avatar';
+import Typography from '@mui/material/Typography';
+import { FullscreenExitRounded } from '@mui/icons-material';
+import { getStorage, ref, getDownloadURL } from 'firebase/storage';
+import { onAuthStateChanged } from 'firebase/auth'
+import { doc, deleteDoc, collection, getDocs, query } from 'firebase/firestore'
+import { db, auth } from "../firebase";
+import { onSnapshot } from 'firebase/firestore';
+
+export default function Profile() {
+    const [authUser, setAuthUser] = useState('')
+    const [firstNameInitial, setFirstNameInitial] = useState('');
+    const [lastNameInitial, setLastNameInitial] = useState('');
+    const [avatarUrl, setAvatarUrl] = useState('')
+    const [firstName, setFirstName] = useState('')
+    const [lastName, setLastName] = useState('')
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    const [wishlistNum, setWishlistNum] = useState(0)
+    const [platforms, setPlatforms] = useState([])
+
+    useEffect(() => {
+        const unsubscribe = onAuthStateChanged(auth, async (user) => {
+          if (user) {
+            setAuthUser(user);
+      
+            let splitUserName = user.displayName.split(' ');
+            let firstName = splitUserName[0];
+            let lastName = splitUserName[1];
+            setFirstName(firstName)
+            setLastName(lastName)
+            setFirstNameInitial(firstName ? firstName[0] : '');
+            setLastNameInitial(lastName ? lastName[0] : '');
+            
+            const storage = getStorage();
+            const avatarRef = ref(storage, 'avatars/' + user.uid);
+      
+            try {
+              const avatarUrl = await getDownloadURL(avatarRef);
+              setAvatarUrl(avatarUrl);
+            } catch (error) {
+              console.error('Error fetching avatar image:', error.message);
+              setAvatarUrl('');
+            }
+            setEmail(user.email);
+          } else {
+            setAuthUser('');
+            setFirstNameInitial('');
+            setLastNameInitial('');
+            setAvatarUrl('');
+            setEmail('');
+          }
+        });
+      
+        return () => {
+          unsubscribe();
+        };
+      }, [auth]);
+
+
+      useEffect(() => {
+        const currentUser = auth.currentUser;
+    
+        if (!currentUser) {
+          return;
+        }
+    
+        const subColRef = collection(db, "users", currentUser.uid, "wishlist");
+        const q = query(subColRef);
+    
+        const unsubscribe = onSnapshot(q, (snapshot) => {
+          const count = snapshot.size;
+          setWishlistNum(count);
+        });
+    
+        return unsubscribe;
+    }, [auth, db]);
+
+
+    useEffect(() => {
+        const currentUser = auth.currentUser;
+      
+        if (!currentUser) {
+          return;
+        }
+      
+        const subColRef = collection(db, "users", currentUser.uid, "platforms");
+        const q = query(subColRef);
+      
+        const unsubscribe = onSnapshot(q, (snapshot) => {
+          const platformData = [];
+      
+          snapshot.forEach((doc) => {
+            platformData.push(doc.data());
+          });
+      
+          setPlatforms(platformData);
+        });
+      
+        return unsubscribe;
+      }, [auth, db]);
+
+  return (
+    <div className="profile-main-body-container">
+      <div className="profile-container">
+        <div className='profile-form-container' style={{
+                display: "flex",
+                flexDirection: "column",
+                justifyContent: "center",
+                alignItems: "flex-start",
+                gap: "20px",
+            }}>
+            <Avatar 
+                sx={{ 
+                    width: 250, 
+                    height: 250, 
+                    margin: "auto",
+                    border: "2px solid black"
+                }} 
+                src={avatarUrl}
+                >
+                {!avatarUrl && `${firstNameInitial}${lastNameInitial}`}
+            </Avatar>
+
+            <Typography variant="body1" gutterBottom sx={{fontSize: "20px"}}>
+                Name: {firstName + " " + lastName}
+            </Typography>
+            <Typography variant="body1" gutterBottom sx={{fontSize: "20px"}}>
+                Email: {email}
+            </Typography>
+            <Typography variant="body1" gutterBottom sx={{fontSize: "20px"}}>
+                Wishlist Items: {wishlistNum}
+            </Typography>
+            <Typography variant="body1" gutterBottom sx={{fontSize: "20px"}}>
+            Platforms: {platforms.map((platform, index) => (
+                <span key={platform.id}>
+                {index > 0 && ', '}
+                {platform.id.charAt(0).toUpperCase() + platform.id.slice(1)}
+                </span>
+            ))}
+            </Typography>
+        </div>
+      </div>
+    </div>
+  )
+}
