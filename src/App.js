24af5d5f5cd141cdff1da3222dcc064fb516a4a8
Kayla Imming <kaylaimming@gmail.com>
2023-06-06 21:06:24 -0700

    Change / route to be conditional on whether user is logged in or not, add states and useEffects to handle that

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
+import { useEffect, useState, useContext } from 'react';
+import { auth } from './firebase';
+
 import './App.css';
-import { Routes, Route, BrowserRouter } from "react-router-dom";
+import { Routes, Route, BrowserRouter, Navigate } from "react-router-dom";
 import Nav from "./components/Nav";
 import Footer from "./components/Footer"
 import Home from "./views/Home";
@@ -12,8 +15,29 @@ import EditProfile from './views/EditProfile';
 import Profile from './views/Profile';
 
 
+import CircularProgress from '@mui/material/CircularProgress';
+import Box from '@mui/material/Box';
+
 
 function App() {
+  const [currentUser, setCurrentUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged(user => {
+      setCurrentUser(user);
+      setLoading(false);
+    });
+
+    // Cleanup subscription on unmount
+    return () => unsubscribe();
+  }, []);
+
+  // if (loading) {
+  //   return <div><CircularProgress /></div>; // Or replace with a loading spinner
+  // }
+
+
   return (
     <div className="App">
       <BrowserRouter>
@@ -21,20 +45,30 @@ function App() {
           <Nav />
 
           <Routes>
-          <Route path="/" element={<Home/>}/>
-          <Route path='/game-search' element={<GameSearch />}/>
-          <Route path="/my-library" element={<MyLibrary />}/>
-          <Route path="/sign-up" element={<SignUp />}/>
-          <Route path="/sign-in" element={<SignIn />}/>
-          <Route path='/wishlist' element={<Wishlist />}/>
-          <Route path='/edit-profile' element={<EditProfile />}/>
-          <Route path='/profile' element={<Profile />}/>
+            <Route
+              path="/"
+              element={
+                currentUser ? (
+                  <Navigate to="/home" replace />
+                ) : (
+                  <Navigate to="/sign-in" replace />
+                )
+              }
+            />
+            <Route path="/home" element={<Home />} />
+            {/* <Route path="/" element={<Home/>}/> */}
+            <Route path="/game-search" element={<GameSearch />} />
+            <Route path="/my-library" element={<MyLibrary />} />
+            <Route path="/sign-up" element={<SignUp />} />
+            <Route path="/sign-in" element={<SignIn />} />
+            <Route path="/wishlist" element={<Wishlist />} />
+            <Route path="/edit-profile" element={<EditProfile />} />
+            <Route path="/profile" element={<Profile />} />
           </Routes>
 
-          <Footer/>
+          <Footer />
         </div>
       </BrowserRouter>
-  
     </div>
   );
 }
