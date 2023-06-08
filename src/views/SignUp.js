9f62ab2caec0c1d83f6317f981ebe656bd044888
Kayla Imming <kaylaimming@gmail.com>
2023-06-08 12:14:09 -0700

    Change Link import from mui to react-router-dom, changed Link href's to 'to', changed copyright Link to github instead of mui

--- src/views/SignUp.js
+++ src/views/SignUp.js
@@ -6,7 +6,7 @@ import CssBaseline from '@mui/material/CssBaseline';
 import TextField from '@mui/material/TextField';
 import FormControlLabel from '@mui/material/FormControlLabel';
 import Checkbox from '@mui/material/Checkbox';
-import Link from '@mui/material/Link';
+// import Link from '@mui/material/Link';
 import Grid from '@mui/material/Grid';
 import Box from '@mui/material/Box';
 import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
@@ -16,14 +16,14 @@ import { createTheme, ThemeProvider } from '@mui/material/styles';
 import { auth, db } from '../firebase'
 import { createUserWithEmailAndPassword, updateProfile, fetchSignInMethodsForEmail } from 'firebase/auth';
 import { doc, setDoc } from "firebase/firestore";
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, Link } from 'react-router-dom';
 
 
 function Copyright(props) {
   return (
     <Typography variant="body2" color="text.secondary" align="center" {...props}>
       {'Copyright © '}
-      <Link color="inherit" href="https://mui.com/">
+      <Link color="inherit" to="https://github.com/kaylajo838">
         Kayla Imming
       </Link>{' '}
       {new Date().getFullYear()}
@@ -246,7 +246,7 @@ export default function SignUp() {
                   </Grid>
                   <Grid container justifyContent="center">
                     <Grid item>
-                      <Link href="/sign-in" variant="body2" sx={{ fontSize: "18px" }}> 
+                      <Link to="/sign-in" variant="body2" sx={{ fontSize: "18px" }}> 
                         Already have an account? Sign in
                       </Link>
                     </Grid>
